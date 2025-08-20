3c8d3c3195d59ac7e0f35b18bddd822c0a528206
sumit kumar <sumit@eazotel.com>
2025-08-20 15:19:40 +0530

    remove about us title

--- src/app/(home)/components/AboutUs.tsx
+++ src/app/(home)/components/AboutUs.tsx
@@ -3,7 +3,6 @@ import { SectionWithContainer } from "@/components";
 import Image from "next/image";
 
 const AboutUs: React.FC<AboutUsPropsType> = ({
-  title,
   subTitle,
   description,
   src,
@@ -16,7 +15,7 @@ const AboutUs: React.FC<AboutUsPropsType> = ({
         </div>
         <div className="flex flex-col gap-6">
           <div className=" space-y-2">
-            <h2 className="text-primary text-lg">{title}</h2>
+            {/* <h2 className="text-primary text-lg">{title}</h2> */}
             <h2 className="font-fr x_lg_font_s text-secondary">{subTitle}</h2>
           </div>
           <div className="w-full relative aspect-[4/2.5] md:hidden block">
